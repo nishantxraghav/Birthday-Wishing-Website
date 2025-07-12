311fdcde92bba2b6b4eaf836abb6141dd1cca28d
Nishant Raghav <nishantxraghav@gmail.com>
2025-07-12 23:50:38 +0530

    Update Surprise.tsx

--- src/components/Surprise.tsx
+++ src/components/Surprise.tsx
@@ -17,7 +17,7 @@ const Surprise = () => {
     "Decorate",
     "Fly the Balloons",
     "Let's Cut the Cake Madam Ji",
-    "Well, I Have a Message for You Madam Ji"
+    "Well, I Have a Message for You Sanya💗"
   ];
 
   const handleClick = () => {
